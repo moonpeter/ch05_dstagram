1dfc00b64f4d64956b0c1aa09ac43dfd336d1542
moonpeter <moonpeter@kakao.com>
2020-05-14 15:16:33 +0900

    update view(list, upload, delete, update) / url 연결 / base.html 작성

---
+++ photo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.views.generic import DetailView
+
+from photo.models import Photo
+from photo.views import photo_list, PhotoUploadView, PhotoDeleteView, PhotoUpdateView
+
+app_name = 'photo'
+
+urlpatterns = [
+    path('', photo_list, name='photo_list'),
+    path('detail/<int:pk>/', DetailView.as_view(model=Photo, template_name='photo/detail.html'), name='photo_detail'),
+    path('upload/', PhotoUploadView.as_view(), name='photo_upload'),
+    path('delete/<int:pk>/', PhotoDeleteView.as_view(), name='photo_detail'),
+    path('update/<int:pk>/', PhotoUpdateView.as_view(), name='photo_update'),
+]
